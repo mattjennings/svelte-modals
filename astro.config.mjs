98229389035acb19a82b155094164ec83ef9ceeb
Matt Jennings <me@mattjennings.io>
2024-10-25 19:44:44 -0500

    fix docs output

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import svelte from '@astrojs/svelte'
 // https://astro.build/config
 export default defineConfig({
   srcDir: 'docs',
+  outDir: 'dist-docs',
   vite: {
     resolve: {
       alias: {
