0dddf02f377030b583072d654afa660e96cefd9b
Matt Jennings <me@mattjennings.io>
2024-11-02 19:49:57 -0500

    docs restructure

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,14 +27,31 @@ export default defineConfig({
       },
       sidebar: [
         {
-          label: 'Usage',
+          label: 'Overview',
           items: [
-            { label: 'Getting Started', slug: 'usage/getting-started' },
-            { label: 'Modal Components', slug: 'usage/modal-components' },
-            { label: '<Modals />', slug: 'usage/modals' },
-            { label: 'API Reference', slug: 'usage/api' },
-            { label: 'v2 Migration', slug: 'usage/v2-migration' }
+            { label: 'Getting Started', slug: 'overview/getting-started' },
+            { label: 'How it works', slug: 'overview/how-it-works' }
           ]
+        },
+        {
+          label: 'Modal Components',
+          items: [
+            { label: 'Props', slug: 'modal-components/props' },
+            { label: 'Transitions', slug: 'modal-components/transitions' },
+            { label: 'Snippets', slug: 'modal-components/snippets' },
+            { label: 'Lazy Loading', slug: 'modal-components/lazy-loading' }
+          ]
+        },
+        {
+          label: 'API',
+          items: [
+            { label: 'modals', slug: 'api/modals' },
+            { label: '<Modals />', slug: 'api/modals-component' }
+          ]
+        },
+        {
+          label: 'Migrations',
+          items: [{ label: 'v2', slug: 'migrations/v2' }]
         }
       ],
       customCss: ['./docs/styles.css']
